49b9caf6e1b73b736eeb112cff879c90e92e002f
SoumyBhow <soumybhow1.0@gmail.com>
2023-05-07 17:50:27 -0400

    Round cals to nearest int

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './container.css';
-import Selection from './Selection.jsx';
+import React from "react";
+import "./container.css";
+import Selection from "./Selection.jsx";
 
 const Container = ({ recipe }) => {
   const { label, calories, ingredients, image } = recipe || {}; // Destructure the recipe object
@@ -8,19 +8,19 @@ const Container = ({ recipe }) => {
   const numOfIngredients = ingredients ? ingredients.length : 0; // Check if ingredients exist before accessing length
 
   return (
-      <div className="card">
-        <div className="img">
-          <img className="foodImg" src={image} alt="food" />
-        </div>
-        <div className="description">
-          <h2 className="recipe">{label}</h2>
-          <div className="info">
-            <p>Calories: {calories} cal</p>
-            <p>No of ingredients: {numOfIngredients}</p>
-          </div>
-          <Selection />
-        </div>
+    <div className="card">
+      <div className="img">
+        <img className="foodImg" src={image} alt="food" />
       </div>
+      <div className="description">
+        <h2 className="recipe">{label}</h2>
+        <div className="info">
+          <p>Calories: {Math.round(calories)} cal</p>
+          <p>No of ingredients: {numOfIngredients}</p>
+        </div>
+        <Selection />
+      </div>
+    </div>
   );
 };
 
