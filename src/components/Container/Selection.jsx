e12d13758a56f6a498ba4c6b4187bd4acf0915f7
SoumyBhow <soumybhow1.0@gmail.com>
2023-05-07 19:13:19 -0400

    Start working on stroke color change

--- src/components/Container/Selection.jsx
+++ src/components/Container/Selection.jsx
@@ -3,17 +3,26 @@ import { ReactComponent as Heart } from "../../statics/heart.svg";
 
 const Selection = () => {
   const [fillColor, setFillColor] = useState("");
+  const [strokeColor, setStrokeColor] = useState("");
   const alter = () => {
     setFillColor((fillColor) =>
       fillColor === "rgba(207, 78, 93, 1)"
         ? "rgba(0,0,0,0)"
         : "rgba(207, 78, 93, 1)"
     );
+    if (fillColor === "rgba(207, 78, 93, 1)") {
+      setStrokeColor(fillColor);
+    } else {
+      setStrokeColor("rgba(0,0,0,1)");
+    }
   };
   return (
     <div className="selection">
       <div className="favourite" onMouseUp={alter}>
-        <Heart style={{ fill: fillColor }} className="heart" />
+        <Heart
+          style={{ fill: fillColor, stroke: strokeColor }}
+          className="heart"
+        />
       </div>
       <div className="explore-con">
         <button className="explore">Explore</button>
