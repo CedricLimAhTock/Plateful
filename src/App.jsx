3c7f0b554eee771dc88dbea8864def2112c437d2
Cedric Lim Ah Tock <c_limaht@live.concordia.ca>
2023-05-18 13:14:05 -0400

    Credits to Edamam's API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Credits from './components/Credits/Credits'
 import Search from './components/Search/Search'
 import Footer from './components/Footer/Footer'
 import './index.css'
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <>
         <Header />
+        <Credits />
         <Search />
         <div className="card-gallery"></div>
         <Footer />
---
+++ src/components/Credits/Credits.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Credits.css'
+
+const Credits = () => {
+  return (
+    <div className="credit-box">This project was created with the use of Edamam's API</div>
+  )
+}
+
+export default Credits
