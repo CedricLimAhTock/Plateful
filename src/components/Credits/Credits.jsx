b5c910607cb9a74b26c8748bad3c5b3f9dfdf1c1
SoumyBhow <soumybhow1.0@gmail.com>
2023-05-18 13:19:59 -0400

    Make credit link to actual api site

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -3,7 +3,7 @@ import './Credits.css'
 
 const Credits = () => {
   return (
-    <div className="credit-box">This project was created with the use of Edamam's API</div>
+    <div className="credit-box">This project was created with the use of <a className="api-link" target="_blank" href="https://www.edamam.com/">Edamam's API</a></div>
   )
 }
 
